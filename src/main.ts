ab00dcf8b44de3b4bd4b8d96b7d43ada93c5f6ba
Artemon0830 <164068609+Artemon0830@users.noreply.github.com>
2024-10-16 20:34:54 +0200

    validator-transformer

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+import { SwaggerHelper } from './common/helpers/swagger.helper';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('March-2024 NestJS')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addBearerAuth({
+      in: 'header',
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerHelper.setDefaultResponses(document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      defaultModelsExpandDepth: 7,
+      persistAuthorization: true,
+    },
+  });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }))
+  const port = 3000;
+  const host = 'localhost';
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+    console.log(`Swagger is running on http://${host}:${port}/docs`);
+  });
+}
+void bootstrap();
---
+++ src/users/dto/req/user-base.req.dto.ts
@@ -0,0 +1,87 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsNotIn,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  Max,
+  Min, Validate, ValidateIf,
+  ValidateNested,
+} from 'class-validator';
+import { transformHelper, TransformHelper } from '../../../common/helpers/transform.helper';
+import { EnumGender } from '../../enums/gender.enum';
+
+export class CarBaseReqDto {
+  @IsString()
+  @Transform(TransformHelper.trim)
+  @Transform(TransformHelper.toLowerCase)
+  @Length(3, 50)
+  producer: string;
+
+  @IsString()
+  @Transform(TransformHelper.trim)
+  @Transform(TransformHelper.toLowerCase)
+  @Length(3, 50)
+  model: string;
+
+  @Type(() => Number)
+  @ApiProperty({example:2022})
+  @IsInt()
+  @Min(1900)
+  @Max(2024)
+  year: number;
+
+}
+export class UserBaseReqDto {
+  @IsString()
+  @Transform(TransformHelper.trim)
+  @Transform(TransformHelper.toLowerCase)
+  @Length(3, 50)
+  name: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  @IsOptional()
+  age?: number;
+
+  @IsString()
+  @Transform(TransformHelper.trim)
+  @Transform(TransformHelper.toLowerCase)
+  @ValidateIf((obj) => !obj.phone )
+  @ApiProperty({example:'example@.com'})
+  @Matches (/@/ )
+  email: string;
+
+  @ValidateIf((obj) => !obj.email )
+  @Transform(TransformHelper.trim)
+  @Transform(TransformHelper.toLowerCase)
+  @IsString()
+  phone: string;
+
+  @IsOptional()
+  @IsEnum(EnumGender)
+  gender: EnumGender;
+
+  @IsBoolean()
+  @Transform(TransformHelper.trim)
+  @IsOptional()
+  isStudent: boolean=false;
+  @IsNotIn(['password','123456789','admin','user','qwerty'])
+  @ApiProperty({example:'fas234AA'})
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+  message: 'Password must contain at least one uppercase letter, one lowercase letter and one number, and at least 8 characters long'
+})
+  password: string;
+
+  @ValidateNested({ each: true})
+  @Type(() => CarBaseReqDto)
+  @IsArray()
+  cars: CarBaseReqDto[];
+}
